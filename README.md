ac44af19d62e8469e7d401b727b98ff7d209d99a
henlin417 <henryling66@gmail.com>
2020-10-30 23:52:32 -0700

    final version

---
+++ README.md
@@ -0,0 +1,15 @@
+This is a clone of Amazon with the following features:
+    - sign in with user authentication
+    - add products to basket
+    - checkout function with credit card
+    - order history 
+
+Technical skills used:
+    - react context api
+    - react JS
+    - Material-UI
+    - Stripe
+    - Node.js
+    - Firebase
+
+Live: https://clone-8888e.web.app
---
+++ build/precache-manifest.3a0165be112916cba3b6a526e77b0a11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c57f8218d42479ca92c356257fc61cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e73707b08c90a2ea54ae",
+    "url": "/static/css/main.43b58dbc.chunk.css"
+  },
+  {
+    "revision": "f2d4e219fcc39f375390",
+    "url": "/static/js/2.82f8dc2b.chunk.js"
+  },
+  {
+    "revision": "4e9e6a426e4934b119d62e02d6fa5a5c",
+    "url": "/static/js/2.82f8dc2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e73707b08c90a2ea54ae",
+    "url": "/static/js/main.f584770c.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
